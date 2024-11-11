aff536ba35cdef54ccaddbcea86fa39254797642
--replace-all <sprhackz003@gmail.com>
2024-11-11 19:15:58 +0530

    feat:seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,28 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "SPR's AI Calculator",
-  description: "AI Powered Drawing Calcultor",
+  description: "AI Powered Drawing Calculator",
+  openGraph: {
+    title: "SPR's AI Calculator",
+    description: "AI-powered calculator built with Next.js 15  and Clerk authentication",
+    url: "https://spr-ai-calculator.vercel.app", // Update with your website URL
+    siteName: "SPR's AI Calculator",
+    images: [
+      {
+        url: "/favicon.png", // Replace with your SEO image path
+        width: 1200,
+        height: 630,
+        alt: "SPR's AI Calculator",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "SPR's AI Calculator",
+    description: "AI-powered calculator built with Next.js 15 and Clerk authentication",
+    images: "/favicon.png", 
+    // Replace with your SEO image path
+  },
 };
 
 export default function RootLayout({
